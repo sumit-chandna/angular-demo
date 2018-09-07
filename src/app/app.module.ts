6b0e02d168d1a6c7efdfe5df075912c18ad5ba16
Sumit Chandna <chandna.sumit@hcentive.com>
2018-09-07 17:50:07 +0530

    7sept18

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -19,23 +19,58 @@ import { PostService } from './services/post.service';
 import { AppErrorHandler } from './common/app-error-handler';
 import { FollowersComponent } from './followers/followers.component';
 import { FollowerService } from './services/follower.service';
+import { NavbarComponent } from './navbar/navbar.component';
+import { GithubProfileComponent } from './github-profile/github-profile.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { HomeComponent } from './home/home.component';
+import { RouterModule } from '@angular/router';
 @NgModule({
   declarations: [
-    AppComponent, CourseComponent, CoursesComponent,
-    SummaryPipe, FavoriteComponent, PanelComponent,
-    InputFormatDirective, ContactFormComponent,
+    AppComponent,
+    CourseComponent,
+    CoursesComponent,
+    SummaryPipe,
+    FavoriteComponent,
+    PanelComponent,
+    InputFormatDirective,
+    ContactFormComponent,
     SignupFormComponent,
     NewcourseformComponent,
     ChangePasswordComponent,
     PostsComponent,
-    FollowersComponent
+    FollowersComponent,
+    NavbarComponent,
+    GithubProfileComponent,
+    NotFoundComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     ReactiveFormsModule,
-    HttpModule
-
+    HttpModule,
+    RouterModule.forRoot([
+      {
+        path: '',
+        component: HomeComponent
+      },
+      {
+        path: 'followers/:username',
+        component: GithubProfileComponent
+      },
+      {
+        path: 'followers',
+        component: FollowersComponent
+      },
+      {
+        path: 'posts',
+        component: PostsComponent
+      },
+      {
+        path: '**',
+        component: NotFoundComponent
+      }
+    ])
   ],
   providers: [CoursesService, PostService, FollowerService, {
     provide: ErrorHandler,
